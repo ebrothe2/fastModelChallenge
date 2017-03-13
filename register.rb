482a3a5aa2459e29473a41ee4ff52a55f609007a
Erica Brotherton <eholmes3@gmail.com>
2017-03-13 07:31:23 -0500

    refactoring for readability

--- register.rb
+++ register.rb
@@ -28,7 +28,7 @@ class Register
 				self.ones << '1'
 				self.total += 1
 			else 
-				puts "Sorry not currently accepting currency of the following amount >> #{bill}"
+				raise ArgumentError.new('Please only enter valid bills, not accepting #{bill}')			
 			end
 		end
 	end
@@ -40,20 +40,97 @@ class Register
 				self.total -= 20
 			elsif bill == 10 && tens?
 				self.tens.delete_at(0)
-				self.total -= 20
+				self.total -= 10
 			elsif bill == 5 && fives?
-				self.tens.delete_at(0)
-				self.total -= 20
+				self.fives.delete_at(0)
+				self.total -= 5
 			elsif bill == 2 && twos?
-				self.tens.delete_at(0)
-				self.total -= 20
+				self.twos.delete_at(0)
+				self.total -= 2
 			elsif bill == 1 && ones?
-				self.tens.delete_at(0)
-				self.total -= 20
+				self.ones.delete_at(0)
+				self.total -= 1
 			else
-				raise ArgumentError, 'sorry do not currently have #{bill}\'s in the cash register'
+				raise ArgumentError.new('sorry do not currently have #{bill}\'s in the cash register') 
 			end
 		end 
+	end
+
+	def makeChange(number)
+		if number > self.total
+			raise ArgumentError.new('Not enough money in register to accomodate change for that amount')
+		end
+		billsToMakeChange = []
+		changeLeftToGive = number
+		if changeLeftToGive/20 >= 1
+			if twenties?
+				idealnumberoftwenties = changeLeftToGive/20
+				if idealnumberoftwenties <= billsInRegister(20)
+					billsToMakeChange += Array.new(billsInRegister(20), 20)
+					changeLeftToGive -= 20 * billsInRegister(20)
+				elsif idealnumberoftwenties > billsInRegister(20)
+					billsToMakeChange += Array.new(billsInRegister(20), 20)
+					changeLeftToGive -= 20 * billsInRegister(20)
+				end
+			end
+		end
+		if changeLeftToGive/10 >= 1
+			if tens?
+				idealnumberoftens = changeLeftToGive/10
+				if idealnumberoftens <= billsInRegister(10)
+					billsToMakeChange += Array.new(billsInRegister(10), 10)
+					changeLeftToGive -= 10 * idealnumberoftens 
+				elsif idealnumberoftens > billsInRegister(10)
+					billsToMakeChange += Array.new(billsInRegister(10), 10)
+					changeLeftToGive -= 10 *billsInRegister(10)
+				end
+			end
+		end
+		if changeLeftToGive/5 >= 1
+			if fives?
+				idealnumberoffives = changeLeftToGive/5
+				if billsInRegister(1) == 0 && changeLeftToGive - (idealnumberoffives*5) == 1 && idealnumberoffives-1 <= billsInRegister(5)
+					billsToMakeChange += Array.new(idealnumberoffives-1, 5)
+					changeLeftToGive -= 5 * (idealnumberoffives-1)
+				elsif idealnumberoffives <= billsInRegister(5)
+					billsToMakeChange += Array.new(idealnumberoffives, 5)
+					changeLeftToGive -= 5 * idealnumberoffives 
+				elsif idealnumberoffives > billsInRegister(5)
+					billsToMakeChange += Array.new(billsInRegister(5), 5)
+					changeLeftToGive -= 5 * billsInRegister(5)
+				end
+			end
+		end
+		if changeLeftToGive/2 >= 1
+			if twos?
+				idealnumberoftwos = changeLeftToGive/2
+				if idealnumberoftwos <= billsInRegister(2)
+					billsToMakeChange += Array.new(idealnumberoftwos, 2)
+					changeLeftToGive -= 2 * idealnumberoftwos
+				elsif idealnumberoftwos > billsInRegister(2)
+					billsToMakeChange += Array.new(billsInRegister(2), 2)
+					changeLeftToGive -= 2 * billsInRegister(2)
+				end
+			end
+		end
+		if changeLeftToGive/1 >= 1
+			if ones?
+				idealnumberofones = changeLeftToGive/1
+				if idealnumberofones <= billsInRegister(1)
+					billsToMakeChange += Array.new(idealnumberofones, 1)
+					changeLeftToGive -= 1 * idealnumberofones
+				elsif idealnumberofones > billsInRegister(1)
+					billsToMakeChange += Array.new(billsInRegister(1), 1)
+					changeLeftToGive -= 1 * billsInRegister(1)
+				end
+			end
+		end
+
+		if changeLeftToGive == 0 
+			payCustomer(billsToMakeChange)
+		else 
+			raise ArgumentError.new('We cannot accomodate change for that amount at this time')
+		end
 	end
 
 	def twenties?
@@ -83,6 +160,22 @@ class Register
 	def ones?
 		if self.ones.length >= 1
 			return true
+		end
+	end
+
+	def billsInRegister(bill)
+		if bill == 20
+			return self.twenties.length
+		elsif bill == 10
+			return self.tens.length
+		elsif bill == 5
+			return self.fives.length
+		elsif bill == 2
+			return self.twos.length
+		elsif bill == 1
+			return self.ones.length
+		else
+			raise ArgumentError.new('Not a valid bill increment')
 		end
 	end
 end
