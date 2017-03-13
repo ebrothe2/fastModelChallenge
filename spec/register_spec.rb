2e133028f8832949d70cb8930ffe6d8a91961488
Erica Brotherton <eholmes3@gmail.com>
2017-03-13 06:21:47 -0500

    initial tests passing

--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -20,4 +20,18 @@ describe Register do
 		register.payRegister([20, 10, 5])
 		expect{register.payCustomer([2])}.to raise_error(ArgumentError)
 	end
+
+	it "makes change if available" do
+		register.payRegister([20, 20, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+		register.payCustomer([20, 10, 10, 10, 10, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+		expect{register.makeChange(11)}.to change{register.total}.by(-11)
+	end
+
+	it "raises an error if register doesn't have correct combination of bills to give change" do
+		register.payRegister([20, 20, 10, 10, 10, 10, 5, 5, 5, 5, 5, 5, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+		register.payCustomer([20, 10, 10, 10, 10, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+		register.makeChange(11)
+		expect{register.makeChange(14)}.to raise_error(ArgumentError)
+	end
+
 end
